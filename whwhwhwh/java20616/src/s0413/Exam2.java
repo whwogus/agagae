7eb95a0fc96857bc4c76487e4722b38a5fbf7fb8
user <user@205-PRO2-07>
2023-04-13 15:51:05 +0900

    aefeafeawfaewfawefawefafeae

--- whwhwhwh/java20616/src/s0413/Exam2.java
+++ whwhwhwh/java20616/src/s0413/Exam2.java
@@ -1,7 +1,7 @@
 package s0413;
 
 public class Exam2 {
-
+	//ackthuwalley exam2_1
 	public static void main(String[] args) {
 		for (int i = 0; i < 3; i++) {
 			System.out.println("1234");
--- whwhwhwh/java20616/src/s0413/Exam2_2.java
+++ whwhwhwh/java20616/src/s0413/Exam2_2.java
@@ -1,7 +1,6 @@
 package s0413;
 
 public class Exam2_2 {
-
 	public static void main(String[] args) {
 		for (int i = 1; i < 5; i++) {
 			for (int a = 1; a < 4; a++) {
